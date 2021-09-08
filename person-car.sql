ad99c58dc26e155f0a6406df53da5106e7da2c3b
xavier-kong <kongxianchengxavier@gmail.com>
2021-09-08 17:03:08 +0800

    person-car

--- person-car.sql
+++ person-car.sql
@@ -1,3 +1,10 @@
+create table car (
+	id BIGSERIAL NOT NULL PRIMARY KEY,
+	make VARCHAR(100) NOT NULL,
+	model VARCHAR(100) NOT NULL,
+	price NUMERIC(19, 2) NOT NULL
+);
+
 create table person (
     id BIGSERIAL NOT NULL PRIMARY KEY,
 	first_name VARCHAR(50) NOT NULL,
@@ -5,14 +12,9 @@ create table person (
 	gender VARCHAR(7) NOT NULL,
 	email VARCHAR(100),
 	date_of_birth DATE NOT NULL,
-	country_of_birth VARCHAR(50) NOT NULL
-);
-
-create table car (
-	id BIGSERIAL NOT NULL PRIMARY KEY,
-	make VARCHAR(100) NOT NULL,
-	model VARCHAR(100) NOT NULL,
-	price NUMERIC(19, 2) NOT NULL
+	country_of_birth VARCHAR(50) NOT NULL,
+  car_id BIGINT REFERENCES car (id),
+  UNIQUE(car_id)
 );
 
 insert into person (first_name, last_name, gender, email, date_of_birth, country_of_birth) values ('Fernanda', 'Beardon', 'Female', 'fernandab@is.gd', '1953-10-28', 'Comoros');
